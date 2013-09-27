c5afb37909ed4df10324c40d5107e51f0ab8e56f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 13:42:49 -0500

    implement parsing of .txt to Flashcard Object

--- flashcard_models.rb
+++ flashcard_models.rb
@@ -1,6 +1,7 @@
+require 'pry'
 class Flashcard
   attr_reader :answer, :question
-  def initialize(answer, question)
+  def initialize(question, answer)
     @answer = answer
     @question = question
   end
@@ -15,8 +16,22 @@ class Stack
   end
 
   def add_cards
-    @flashcards << Flashcard.new("Feline", "Stupid name for cat")
+   all_lines = []
+    File.open('flashcard_samples.txt').each do |line|
+      all_lines << line
+    end
+    all_answer_and_deffinitions_and_spaces = all_lines.each_slice(3).to_a
+    all_answer_and_deffinitions_and_spaces.each do |x|
+      x.pop if x[-1] == "\n"
+      @flashcards << Flashcard.new(x[0],x[1])
+    end
   end
+
+
+
+  # something.each do |x|
+  #   @flashcards << Flashcard.new(x)
+  # end
 end
 
 
