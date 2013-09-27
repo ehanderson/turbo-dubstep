88cd8786c4bb4c2bd393a683e6e3bd6f22b43e77
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 11:34:31 -0500

    made a card

--- flashcard_models.rb
+++ flashcard_models.rb
@@ -1,8 +1,21 @@
 class Flashcard
+  attr_reader :answer, :question
+  def initialize(answer, question)
+    @answer = answer
+    @question = question
 end
 
 
 class Stack
+  attr_reader :flashcards
+  def initialize
+    @flashcards = []
+    add_cards
+  end
+
+  def add_cards
+    @flashcards = Flashcard.new("Feline", "Stupid name for cat")
+  end
 end
 
 
