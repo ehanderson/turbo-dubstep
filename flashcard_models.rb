4cc18cc235196c0cdca85bec8d19a5c35c5216ed
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 14:14:28 -0500

    added a permanent shuffle method

--- flashcard_models.rb
+++ flashcard_models.rb
@@ -1,11 +1,5 @@
 require 'pry'
-class Flashcard
-  attr_reader :answer, :question
-  def initialize(question, answer)
-    @answer = answer
-    @question = question
-  end
-end
+
 
 
 class Stack
@@ -20,21 +14,43 @@ class Stack
     File.open('flashcard_samples.txt').each do |line|
       all_lines << line
     end
-    all_answer_and_deffinitions_and_spaces = all_lines.each_slice(3).to_a
-    all_answer_and_deffinitions_and_spaces.each do |x|
+    all_answer_and_definitions_and_spaces = all_lines.each_slice(3).to_a
+    all_answer_and_definitions_and_spaces.each do |x|
       x.pop if x[-1] == "\n"
       @flashcards << Flashcard.new(x[0],x[1])
     end
   end
 
-
+  def serve
+    flashcards.shift
+  end
+  
+  def shuffle!
+    flashcards = flashcards.shuffle
+  end
 
   # something.each do |x|
   #   @flashcards << Flashcard.new(x)
   # end
 end
 
+class Flashcard
+  attr_reader :answer, :question
+  def initialize(question, answer)
+    @answer = answer
+    @question = question
+  end
+
+  # def answer.to_s
+  #   "#{answer}"
+  # end
+
+  # def question.to_s
+  #   "#{question}"
+  # end
+end
 
 
 
   #fuckshitup
+stack = Stack.new
